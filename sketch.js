7e6383529053aef607d99c65c99abe27c7df2b83
mbmotorcyclesproject <myrtlebeachmotorcycleproject@gmail.com>
2025-04-18 11:12:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const wavesurfer = WaveSurfer.create({
   container: document.body,
   waveColor: 'rgb(200, 0, 200)',
   progressColor: 'rgb(100, 0, 100)',
-  url: '07020209.wav',
+  url: 'https://raw.githubusercontent.com/mbmotorcyclesproject/wavesurfer/main/07020209.wav',
 })
 
 wavesurfer.on('click', () => wavesurfer.playPause())
