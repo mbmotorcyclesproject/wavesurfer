e3c104c2275d9045265ebb30b1da0f6c2897447f
mbmotorcyclesproject <myrtlebeachmotorcycleproject@gmail.com>
2025-04-18 11:10:03 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+import WaveSurfer from 'wavesurfer.js'
+
+const wavesurfer = WaveSurfer.create({
+  container: document.body,
+  waveColor: 'rgb(200, 0, 200)',
+  progressColor: 'rgb(100, 0, 100)',
+  url: '/examples/audio/demo.wav',
+})
+
+wavesurfer.on('click', () => wavesurfer.playPause())
+
+wavesurfer.once('play', () => {
+})
